05d3c493add4fec8e06673a4dfb997f9186b4a05
Joao Sousa <joaosousa.pt@gmail.com>
2020-02-12 02:03:04 +0000

    Closer To Be Working. Kinda gets to the endpoint.

--- breadthFirstSearch.py
+++ breadthFirstSearch.py
@@ -1,5 +1,5 @@
 from queue import Queue
-
+from copy import deepcopy
 
 
 def create_maze():
@@ -8,39 +8,50 @@ def create_maze():
     maze.append(['.', ' ', ' ', ' ', '.'])
     maze.append(['.', ' ', '.', '.', '.'])
     maze.append(['.', ' ', ' ', ' ', '.'])
-    maze.append(['.', 'X', '.', '.', '.'])
+    maze.append(['.', 'x', '.', '.', '.'])
 
     return maze
 
-def start_end(maze):
+
+def find_start_end(maze):
 
     for i in range(len(maze)):
         for j in range(len(maze[0])):
             if (maze[i][j] == 'o'):
                 start_coords = [i,j]
-            elif (maze[i][j] == 'X'):
+            elif (maze[i][j] == 'x'):
                 end_coords = [i,j]
     
     return (start_coords, end_coords)
 
-def check_end(current, coords):
-    pos = coords[0] # start position
+def check_move(maze, current, coords):
+    pos = deepcopy(coords[0]) # start position
     for c in current:
-        if (c == 'U'): pos[1] += 1
-        elif (c == 'D'): pos[1] -= 1
-        elif (c == 'R'): pos[0] += 1
-        elif (c == 'L'): pos[0] -= 1
+        if (c == 'U'): pos[0] -= 1
+        elif (c == 'D'): pos[0] += 1
+        elif (c == 'R'): pos[1] += 1
+        elif (c == 'L'): pos[1] -= 1
     
-    return (pos == coords[1])
+    if (pos[0] < 0 or pos[0] > len(maze[0]) or pos[1] < 0 or pos[1] > len(maze)):
+        return False
 
-def is_valid(elem_to_test, maze):
+    if (pos == coords[1]):
+        maze[coords[1][0]][coords[1][1]] = 'X' # X -> Found end with current path; x -> end not yet found
+    
+    elif (maze[pos[0]][pos[1]] != ' ' and maze[pos[0]][pos[1]] != 'o'):
+        print(maze[pos[0]][pos[1]])
+        return False
+    
+    if (maze[pos[0]][pos[1]] == 'o'):
+        maze[pos[0]][pos[1]] = 'O'
+    
+    print("VALID")
     return True
 
+        
 
-# function complete ?
 def solve_maze(maze):
-
-    coords = start_end(maze)
+    coords = find_start_end(maze)
     start = coords[0]
     end = coords[1]
 
@@ -53,7 +64,7 @@ def solve_maze(maze):
     while True:
         current = q.get()
 
-        if (check_end(current, coords)):
+        if (maze[coords[1][0]][coords[1][1]] == 'X'):
             print(current)
             break
 
@@ -61,21 +72,21 @@ def solve_maze(maze):
         for i in ['U', 'D', 'L', 'R']:
             new_elem = current + i
             print(new_elem)
-            if (is_valid(new_elem, maze)):
+            if (check_move(maze, new_elem, coords)):
                 q.put(new_elem)
 
 
-def show_sol(maze, sol):
+def show_sol(maze, sol, coords):
     pass
 
 
 
 
-# if __name__ == "__main__":
-#     maze = create_maze()
-#     solve_maze(maze)
+if __name__ == "__main__":
+    maze = create_maze()
+    solve_maze(maze)
 
-print(check_end('RUUU', [[2,1], [3,4]]))
+#print(check_end('RUUU', [[2,1], [3,4]]))
 
 
 
