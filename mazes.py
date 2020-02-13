c6439c8dcd4eb2223ecc6dbb5a25e2e54fb1ef22
Joao Sousa <joaosousa.pt@gmail.com>
2020-02-13 01:40:44 +0000

    Create mazes.py

---
+++ mazes.py
@@ -0,0 +1,52 @@
+def create_maze_1():
+    maze = []
+    
+    maze.append(['.', '.', '.', 'o', '.'])
+    maze.append(['.', ' ', ' ', ' ', '.'])
+    maze.append(['.', ' ', '.', '.', '.'])
+    maze.append(['.', ' ', ' ', ' ', '.'])
+    maze.append(['.', 'x', '.', '.', '.'])
+
+    return maze
+
+
+def create_maze_2():
+    maze = []
+
+    maze.append(['.', '.', '.', '.', '.'])
+    maze.append(['o', ' ', ' ', ' ', '.'])
+    maze.append(['.', ' ', '.', ' ', '.'])
+    maze.append(['.', ' ', ' ', ' ', '.'])
+    maze.append(['.', '.', 'x', '.', '.'])
+
+    return maze
+
+
+def create_maze_3():
+    maze = []
+    
+    maze.append(['.', '.', '.', 'o', '.', '.'])
+    maze.append(['.', ' ', ' ', ' ', ' ', '.'])
+    maze.append(['.', ' ', '.', '.', ' ', '.'])
+    maze.append(['.', ' ', ' ', ' ', ' ', '.'])
+    maze.append(['.', ' ', ' ', ' ', ' ', '.'])
+    maze.append(['.', '.', '.', 'x', '.', '.'])
+
+    return maze
+
+
+def create_maze_4():
+    maze = []
+
+    maze.append(['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'])
+    maze.append(['.', ' ', ' ', ' ', '.', '.', ' ', '.', ' ', 'o'])
+    maze.append(['.', '.', '.', '.', '.', '.', '.', '.', ' ', '.'])
+    maze.append(['.', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.'])
+    maze.append(['.', ' ', '.', ' ', '.', '.', ' ', '.', ' ', '.'])
+    maze.append(['.', ' ', '.', ' ', '.', '.', ' ', ' ', ' ', '.'])
+    maze.append(['.', ' ', '.', ' ', '.', '.', '.', '.', ' ', '.'])
+    maze.append(['.', ' ', '.', ' ', '.', '.', ' ', ' ', ' ', '.'])
+    maze.append(['.', ' ', '.', ' ', ' ', ' ', ' ', '.', ' ', '.'])
+    maze.append(['.', 'x', '.', '.', '.', '.', '.', '.', '.', '.'])
+
+    return maze
